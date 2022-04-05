d9ac3a634658578445a7e0c6fff8641742d4d718
Leonid-Nenkin <nenkin.leonid@gmail.com>
2022-04-05 11:57:22 +0300

    task1

--- hw1/main.go
+++ hw1/main.go
@@ -1,215 +1,151 @@
 package main
 
 import (
-	"context"
-	"io"
-	"log"
-	"net/http"
+	"fmt"
 	"os"
-	"os/signal"
-	"sync"
-	"syscall"
+	"path/filepath"
+	"log"
+	"context"
 	"time"
-
-	"github.com/PuerkitoBio/goquery"
+	"syscall"
+	"os/signal"
+	"sync/atomic"
 )
 
-type CrawlResult struct {
-	Err   error
-	Title string
-	Url   string
+type TargetFile struct {
+	Path string
+	Name string
 }
 
-type Page interface {
-	GetTitle() string
-	GetLinks() []string
+type FileList map[string]TargetFile
+
+type FileInfo interface {
+	os.FileInfo
+	Path() string
 }
 
-type page struct {
-	doc *goquery.Document
+type fileInfo struct {
+	os.FileInfo
+	path string
 }
 
-func NewPage(raw io.Reader) (Page, error) {
-	doc, err := goquery.NewDocumentFromReader(raw)
-	if err != nil {
-		return nil, err
+func (fi fileInfo) Path() string {
+	return fi.path
+}
+
+func ListDirectory(ctx context.Context, dir string, depth int, maint uint32) ([]FileInfo, error) {
+	if depth <= 0 {
+		return nil, nil
 	}
-	return &page{doc: doc}, nil
-}
 
-func (p *page) GetTitle() string {
-	return p.doc.Find("title").First().Text()
-}
-
-func (p *page) GetLinks() []string {
-	var urls []string
-	p.doc.Find("a").Each(func(_ int, s *goquery.Selection) {
-		url, ok := s.Attr("href")
-		if ok {
-			urls = append(urls, url)
-		}
-	})
-	return urls
-}
-
-type Requester interface {
-	Get(ctx context.Context, url string) (Page, error)
-}
-
-type requester struct {
-	timeout time.Duration
-}
-
-func NewRequester(timeout time.Duration) requester {
-	return requester{timeout: timeout}
-}
-
-func (r requester) Get(ctx context.Context, url string) (Page, error) {
 	select {
 	case <-ctx.Done():
 		return nil, nil
 	default:
-		cl := &http.Client{
-			Timeout: r.timeout,
-		}
-		req, err := http.NewRequest("GET", url, nil)
+		// time.Sleep(time.Second * 10)
+		var result []FileInfo
+		res, err := os.ReadDir(dir)
 		if err != nil {
 			return nil, err
 		}
-		body, err := cl.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer body.Body.Close()
-		page, err := NewPage(body.Body)
-		if err != nil {
-			return nil, err
-		}
-		return page, nil
-	}
-	return nil, nil
-}
 
-//Crawler - интерфейс (контракт) краулера
-type Crawler interface {
-	Scan(ctx context.Context, url string, depth int)
-	ChanResult() <-chan CrawlResult
-}
+		for _, entry := range res {
+			path := filepath.Join(dir, entry.Name())
 
-type crawler struct {
-	r       Requester
-	res     chan CrawlResult
-	visited map[string]struct{}
-	mu      sync.RWMutex
-}
+			if atomic.LoadUint32(&maint) == 1 {
+				fmt.Println(path, depth)
+			}
+			if atomic.LoadUint32(&maint) == 0 {
+				depth+=2
+			}
 
-func NewCrawler(r Requester) *crawler {
-	return &crawler{
-		r:       r,
-		res:     make(chan CrawlResult),
-		visited: make(map[string]struct{}),
-		mu:      sync.RWMutex{},
-	}
-}
-
-func (c *crawler) Scan(ctx context.Context, url string, depth int) {
-	if depth <= 0 { //Проверяем то, что есть запас по глубине
-		return
-	}
-	c.mu.RLock()
-	_, ok := c.visited[url] //Проверяем, что мы ещё не смотрели эту страницу
-	c.mu.RUnlock()
-	if ok {
-		return
-	}
-	select {
-	case <-ctx.Done(): //Если контекст завершен - прекращаем выполнение
-		return
-	default:
-		page, err := c.r.Get(ctx, url) //Запрашиваем страницу через Requester
-		if err != nil {
-			c.res <- CrawlResult{Err: err} //Записываем ошибку в канал
-			return
-		}
-		c.mu.Lock()
-		c.visited[url] = struct{}{} //Помечаем страницу просмотренной
-		c.mu.Unlock()
-		c.res <- CrawlResult{ //Отправляем результаты в канал
-			Title: page.GetTitle(),
-			Url:   url,
-		}
-		for _, link := range page.GetLinks() {
-			go c.Scan(ctx, link, depth-1) //На все полученные ссылки запускаем новую рутину сборки
-		}
-	}
-}
-
-func (c *crawler) ChanResult() <-chan CrawlResult {
-	return c.res
-}
-
-//Config - структура для конфигурации
-type Config struct {
-	MaxDepth   int
-	MaxResults int
-	MaxErrors  int
-	Url        string
-	Timeout    int //in seconds
-}
-
-func main() {
-
-	cfg := Config{
-		MaxDepth:   3,
-		MaxResults: 10,
-		MaxErrors:  5,
-		Url:        "https://telegram.org",
-		Timeout:    10,
-	}
-	var cr Crawler
-	var r Requester
-
-	r = NewRequester(time.Duration(cfg.Timeout) * time.Second)
-	cr = NewCrawler(r)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	go cr.Scan(ctx, cfg.Url, cfg.MaxDepth) //Запускаем краулер в отдельной рутине
-	go processResult(ctx, cancel, cr, cfg) //Обрабатываем результаты в отдельной рутине
-
-	sigCh := make(chan os.Signal)        //Создаем канал для приема сигналов
-	signal.Notify(sigCh, syscall.SIGINT) //Подписываемся на сигнал SIGINT
-	for {
-		select {
-		case <-ctx.Done(): //Если всё завершили - выходим
-			return
-		case <-sigCh:
-			cancel() //Если пришёл сигнал SigInt - завершаем контекст
-		}
-	}
-}
-
-func processResult(ctx context.Context, cancel func(), cr Crawler, cfg Config) {
-	var maxResult, maxErrors = cfg.MaxResults, cfg.MaxErrors
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case msg := <-cr.ChanResult():
-			if msg.Err != nil {
-				maxErrors--
-				log.Printf("crawler result return err: %s\n", msg.Err.Error())
-				if maxErrors <= 0 {
-					cancel()
-					return
+			if entry.IsDir() {
+				child, err := ListDirectory(ctx, path, depth-1, maint)
+				if err != nil {
+					return nil, err
 				}
+				result = append(result, child...)
 			} else {
-				maxResult--
-				log.Printf("crawler result: [url: %s] Title: %s\n", msg.Url, msg.Title)
-				if maxResult <= 0 {
-					cancel()
-					return
+				info, err := entry.Info()
+				if err == nil {
+					result = append(result, fileInfo{info,path})
 				}
 			}
 		}
+	return result, err
 	}
+}
+
+func FindFiles(ctx context.Context, ext string, maxDepth int, maint uint32) (FileList, error) {	
+	wd, err := os.Getwd()
+	
+	if err != nil {
+		return nil, err
+	}
+
+	files, err := ListDirectory(ctx, wd, maxDepth, maint)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fl := make(FileList, len(files))
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ext {
+			fl[file.Name()] = TargetFile{
+				Name: file.Name(),
+				Path: file.Path(),
+			}
+		}
+	}
+	return fl, nil
+}
+
+type Config struct {
+	MaxDepth   int
+}
+
+func main() {
+	cfg := Config{2}
+	const wantExt = ".go"
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout (ctx, 30 * time.Second)
+	defer cancel()
+
+	sigCh := make(chan os.Signal, 1)
+	var maint uint32
+	go func() {
+        for sig := range sigCh {
+            switch sig {
+            case syscall.SIGUSR1:
+                atomic.StoreUint32(&maint, 1)
+            case syscall.SIGUSR2:
+                atomic.StoreUint32(&maint, 0)
+            }
+        }
+    }()
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGUSR2)
+	
+	waitCh := make(chan struct{})
+	go func() {
+		res, err := FindFiles(ctx, wantExt, cfg.MaxDepth, maint)
+	
+		if err != nil {
+			log.Printf("Error on search: %v/n", err)
+			os.Exit(1)
+		}
+	
+		for _, f := range res {
+			fmt.Printf("Name: %s\t\t Path: %s\n", f.Name, f.Path)
+		}
+		waitCh <- struct{}{}
+	}()
+	go func() {
+		<- sigCh
+		log.Println("Signal received, terminate")	
+		cancel()
+	}()
+	
+	<-waitCh
+	log.Println("Done")
 }
